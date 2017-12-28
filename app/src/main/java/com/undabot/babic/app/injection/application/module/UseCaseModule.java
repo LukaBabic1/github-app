a12dd44637ba4639e79bea73e1ed4f80eb817ee4
Luka <luka.babic@fivemintes.eu>
2017-12-28 17:21:37 +0100

    Exposing usecases from modules.

--- app/src/main/java/com/undabot/babic/app/injection/application/module/UseCaseModule.java
+++ app/src/main/java/com/undabot/babic/app/injection/application/module/UseCaseModule.java
@@ -1,11 +1,31 @@
 package com.undabot.babic.app.injection.application.module;
 
+import com.undabot.babic.domain.repository.CodeRepositoryRepository;
+import com.undabot.babic.domain.usecase.SearchMoreRepositoriesUseCase;
+import com.undabot.babic.domain.usecase.SearchMoreRepositoriesUseCaseImpl;
+import com.undabot.babic.domain.usecase.SearchRepositoriesUseCase;
+import com.undabot.babic.domain.usecase.SearchRepositoriesUseCaseImpl;
+
 import dagger.Module;
+import dagger.Provides;
 
 @Module
 public final class UseCaseModule {
 
+    @Provides
+    SearchRepositoriesUseCase provideSearchrRepositoriesUseCase(final CodeRepositoryRepository codeRepositoryRepository) {
+        return new SearchRepositoriesUseCaseImpl(codeRepositoryRepository);
+    }
+
+    @Provides
+    SearchMoreRepositoriesUseCase provideSearchMoreRepositoriesUseCase(final CodeRepositoryRepository codeRepositoryRepository) {
+        return new SearchMoreRepositoriesUseCaseImpl(codeRepositoryRepository);
+    }
+
     public interface Exposes {
 
+        SearchRepositoriesUseCase searchRepositoriesUseCase();
+
+        SearchMoreRepositoriesUseCase searchMoreRepositoriesUseCase();
     }
 }
