11bd1ac3802c47ac1a761eb3a078a1a43e798107
Luka <luka.babic@fivemintes.eu>
2017-12-28 10:09:19 +0100

    Adding CodeRepository API model.

---
+++ data/src/main/java/com/undabot/babic/data/network/model/ApiCodeRepository.java
@@ -0,0 +1,66 @@
+package com.undabot.babic.data.network.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public final class ApiCodeRepository {
+
+    @SerializedName("id")
+    public int id;
+
+    @SerializedName("name")
+    public String name;
+
+    @SerializedName("full_name")
+    public String fullName;
+
+    @SerializedName("owner")
+    public ApiUser owner;
+
+    @SerializedName("description")
+    public String description;
+
+    @SerializedName("url")
+    public String repositoryUrl;
+
+    @SerializedName("html_url")
+    public String homepageUrl;
+
+    @SerializedName("language")
+    public String language;
+
+    @SerializedName("created_at")
+    public long createdAt;
+
+    @SerializedName("updated_at")
+    public long updatedAt;
+
+    @SerializedName("pushed_at")
+    public long pushedAt;
+
+    @SerializedName("stargazers_count")
+    public int stargazersCount;
+
+    @SerializedName("watchers_count")
+    public int watchersCount;
+
+    @SerializedName("forks_count")
+    public int forksCount;
+
+    @SerializedName("open_issues_count")
+    public int openIssuesCount;
+
+    @SerializedName("score")
+    public float score;
+
+    @SerializedName("private")
+    public boolean isPrivate;
+
+    @SerializedName("has_issues")
+    public boolean hasIssues;
+
+    @SerializedName("has_projects")
+    public boolean hasProjects;
+
+    @SerializedName("has_wiki")
+    public boolean hasWiki;
+}
