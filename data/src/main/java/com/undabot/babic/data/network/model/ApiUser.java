e6d0ff80bb8b11c3fb82b07dc24a1b07ffec721b
Luka <luka.babic@fivemintes.eu>
2018-01-03 14:15:23 +0100

    Adding explanations for ApiUser.

--- data/src/main/java/com/undabot/babic/data/network/model/ApiUser.java
+++ data/src/main/java/com/undabot/babic/data/network/model/ApiUser.java
@@ -49,6 +49,9 @@ public final class ApiUser {
     @SerializedName("updated_at")
     public String updatedAt;
 
+    /**
+     * Only available for authorized current user.
+     */
     @SerializedName("private_gists")
     public int privateGists;
 
@@ -58,48 +61,93 @@ public final class ApiUser {
     @SerializedName("public_gists")
     public int publicGists;
 
+    /**
+     * Only available for authorized current user.
+     */
     @SerializedName("total_private_repos")
     public int totalPrivateRepos;
 
+    /**
+     * Only available for authorized current user.
+     */
     @SerializedName("owned_private_repos")
     public int ownedPrivateRepos;
 }
 
+
 /*
-            "login": "LukaBabic1",
-            "id": 4068512,
-            "avatar_url": "https://avatars0.githubusercontent.com/u/4068512?v=4",
-            "gravatar_id": "",
-            "url": "https://api.github.com/users/LukaBabic1",
-            "html_url": "https://github.com/LukaBabic1",
-            "followers_url": "https://api.github.com/users/LukaBabic1/followers",
-            "following_url": "https://api.github.com/users/LukaBabic1/following{/other_user}",
-            "gists_url": "https://api.github.com/users/LukaBabic1/gists{/gist_id}",
-            "starred_url": "https://api.github.com/users/LukaBabic1/starred{/owner}{/repo}",
-            "subscriptions_url": "https://api.github.com/users/LukaBabic1/subscriptions",
-            "organizations_url": "https://api.github.com/users/LukaBabic1/orgs",
-            "repos_url": "https://api.github.com/users/LukaBabic1/repos",
-            "events_url": "https://api.github.com/users/LukaBabic1/events{/privacy}",
-            "received_events_url": "https://api.github.com/users/LukaBabic1/received_events",
-            "type": "User",
-            "site_admin": false,
-            "name": "Luka Babić",
-            "company": "Five",
-            "blog": "",
-            "location": "Zagreb",
-            "email": null,
-            "hireable": null,
-            "bio": null,
-            "public_repos": 7,
-            "public_gists": 0,
-            "followers": 0,
-            "following": 0,
-            "created_at": "2013-04-05T11:56:55Z",
-            "updated_at": "2017-12-08T18:27:44Z",
-            "private_gists": 0,
-            "total_private_repos": 0,
-            "owned_private_repos": 0,
-            "disk_usage": 985,
-            "collaborators": 0,
-            "two_factor_authentication": false,
+    Authorized user response example.
+
+        "login": "LukaBabic1",
+        "id": 4068512,
+        "avatar_url": "https://avatars0.githubusercontent.com/u/4068512?v=4",
+        "gravatar_id": "",
+        "url": "https://api.github.com/users/LukaBabic1",
+        "html_url": "https://github.com/LukaBabic1",
+        "followers_url": "https://api.github.com/users/LukaBabic1/followers",
+        "following_url": "https://api.github.com/users/LukaBabic1/following{/other_user}",
+        "gists_url": "https://api.github.com/users/LukaBabic1/gists{/gist_id}",
+        "starred_url": "https://api.github.com/users/LukaBabic1/starred{/owner}{/repo}",
+        "subscriptions_url": "https://api.github.com/users/LukaBabic1/subscriptions",
+        "organizations_url": "https://api.github.com/users/LukaBabic1/orgs",
+        "repos_url": "https://api.github.com/users/LukaBabic1/repos",
+        "events_url": "https://api.github.com/users/LukaBabic1/events{/privacy}",
+        "received_events_url": "https://api.github.com/users/LukaBabic1/received_events",
+        "type": "User",
+        "site_admin": false,
+        "name": "Luka Babić",
+        "company": "Five",
+        "blog": "",
+        "location": "Zagreb",
+        "email": null,
+        "hireable": null,
+        "bio": null,
+        "public_repos": 7,
+        "public_gists": 0,
+        "followers": 0,
+        "following": 0,
+        "created_at": "2013-04-05T11:56:55Z",
+        "updated_at": "2017-12-08T18:27:44Z",
+        "private_gists": 0,
+        "total_private_repos": 0,
+        "owned_private_repos": 0,
+        "disk_usage": 985,
+        "collaborators": 0,
+        "two_factor_authentication": false,
+*/
+
+
+/*
+    Unauthorized user response example.
+
+        login: "JakeWharton",
+        id: 66577,
+        avatar_url: "https://avatars0.githubusercontent.com/u/66577?v=4",
+        gravatar_id: "",
+        url: "https://api.github.com/users/JakeWharton",
+        html_url: "https://github.com/JakeWharton",
+        followers_url: "https://api.github.com/users/JakeWharton/followers",
+        following_url: "https://api.github.com/users/JakeWharton/following{/other_user}",
+        gists_url: "https://api.github.com/users/JakeWharton/gists{/gist_id}",
+        starred_url: "https://api.github.com/users/JakeWharton/starred{/owner}{/repo}",
+        subscriptions_url: "https://api.github.com/users/JakeWharton/subscriptions",
+        organizations_url: "https://api.github.com/users/JakeWharton/orgs",
+        repos_url: "https://api.github.com/users/JakeWharton/repos",
+        events_url: "https://api.github.com/users/JakeWharton/events{/privacy}",
+        received_events_url: "https://api.github.com/users/JakeWharton/received_events",
+        type: "User",
+        site_admin: false,
+        name: "Jake Wharton",
+        company: "Google, Inc.",
+        blog: "http://jakewharton.com",
+        location: "Pittsburgh, PA, USA",
+        email: null,
+        hireable: null,
+        bio: null,
+        public_repos: 93,
+        public_gists: 54,
+        followers: 42664,
+        following: 12,
+        created_at: "2009-03-24T16:09:53Z",
+        updated_at: "2017-12-27T15:05:55Z"
 */
