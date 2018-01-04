3e7459ce57d553b395cb80e4315f1ec21f725f55
Luka <luka.babic@fivemintes.eu>
2018-01-04 12:59:41 +0100

    Adding api acces token response.

---
+++ data/src/main/java/com/undabot/babic/data/network/model/ApiAccessTokenResponse.java
@@ -0,0 +1,9 @@
+package com.undabot.babic.data.network.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public final class ApiAccessTokenResponse {
+
+    @SerializedName("access_token")
+    public String accessToken;
+}
