232af9b2acf05a850a8edba6a783fe27a2e53405
Luka <luka.babic@fivemintes.eu>
2018-01-04 11:58:23 +0100

    Adding LoginViewModel.

---
+++ app/src/main/java/com/undabot/babic/app/ui/login/LoginViewModel.java
@@ -0,0 +1,10 @@
+package com.undabot.babic.app.ui.login;
+
+public final class LoginViewModel {
+
+    public final String webViewAuthorizationUrl;
+
+    public LoginViewModel(final String webViewAuthorizationUrl) {
+        this.webViewAuthorizationUrl = webViewAuthorizationUrl;
+    }
+}
