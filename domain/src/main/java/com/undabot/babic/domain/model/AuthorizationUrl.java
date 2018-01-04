940a8514ca1bcf2a3975b82ec2d344c8027b84b1
Luka <luka.babic@fivemintes.eu>
2018-01-04 11:58:15 +0100

    Adding AuthorizationUrl

---
+++ domain/src/main/java/com/undabot/babic/domain/model/AuthorizationUrl.java
@@ -0,0 +1,29 @@
+package com.undabot.babic.domain.model;
+
+public final class AuthorizationUrl {
+
+    public final String value;
+
+    public AuthorizationUrl(final String value) {
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        final AuthorizationUrl that = (AuthorizationUrl) o;
+
+        return value != null ? value.equals(that.value) : that.value == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return value != null ? value.hashCode() : 0;
+    }
+}
