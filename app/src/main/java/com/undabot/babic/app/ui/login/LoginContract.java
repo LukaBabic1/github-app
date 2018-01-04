b66e59635283baca0db546a63d3bf6121377eeb8
Luka <luka.babic@fivemintes.eu>
2018-01-04 11:58:32 +0100

    Adding LoginContract.

---
+++ app/src/main/java/com/undabot/babic/app/ui/login/LoginContract.java
@@ -0,0 +1,21 @@
+package com.undabot.babic.app.ui.login;
+
+import com.undabot.babic.app.base.BaseView;
+import com.undabot.babic.app.base.ScopedPresenter;
+
+public final class LoginContract {
+
+    private LoginContract() {
+
+    }
+
+    public interface View extends BaseView {
+
+        void render(LoginViewModel viewModel);
+    }
+
+    public interface Presenter extends ScopedPresenter {
+
+        void init();
+    }
+}
